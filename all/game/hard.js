7cb6f17bd8ae8bdcf33ff1599bc0ab83b1b4058f
ZOXSEM <124510677+ZOXSEM@users.noreply.github.com>
2023-02-05 21:40:33 +0700

    Add files via upload

---
+++ all/game/hard.js
@@ -0,0 +1,141 @@
+var ps = 15;
+
+function nfp(urpx) {
+    return Number(urpx.replace("px", ""))
+}
+
+var r = document.getElementById('right');
+var l = document.getElementById('left');
+var b = document.getElementById('ball');
+
+var rscore = document.getElementById('scoreleft');
+var lscore = document.getElementById('scoreright');
+var ogoal = document.getElementById('goal');
+
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+var map = [];
+onkeydown = onkeyup = function(e) {
+    e = e || event;
+    map[e.keyCode] = e.type == 'keydown';
+    /*insert conditional here*/
+}
+
+
+
+function keydown() {
+    if (map[40]) {
+        if (nfp(r.style.top) + ps > h - 200)
+            r.style.top = h - 200 + "px";
+        else
+            r.style.top = nfp(r.style.top) + ps + "px";
+    }
+
+
+
+    else if (map[38]) {
+        if (nfp(r.style.top) - ps < 0)
+            r.style.top = 0 + "px";
+        else
+            r.style.top = nfp(r.style.top) - ps + "px";
+    }
+
+
+    if (map[83]) {
+        if (nfp(l.style.top) + ps > h - 200)
+            l.style.top = h - 200 + "px";
+        else
+            l.style.top = nfp(l.style.top) + ps + "px";
+    }
+
+    else if (map[87]) {
+        if (nfp(l.style.top) - ps < 0)
+            l.style.top = 0 + "px";
+        else
+            l.style.top = nfp(l.style.top) - ps + "px";
+    }
+}
+
+
+var speedx = 12,
+    speedy = 4;
+var balltime = 0.25;
+b.style.left = w / 2 + "px";
+
+function ball() {
+    b.style.left = nfp(b.style.left) + speedx + "px";
+    b.style.top = nfp(b.style.top) + speedy + "px";
+}
+
+
+
+
+function moveball() {
+    ball();
+
+    if (h < nfp(b.style.top) + 20 || nfp(b.style.top) < 0) {
+        speedy *= -1;
+        var audio = new Audio();
+        audio.src = 'ping.mp3';
+        audio.autoplay = true;
+    }
+
+    if (nfp(b.style.left) >= w - 50) {
+        if (nfp(r.style.top) <= nfp(b.style.top) + 20 && nfp(r.style.top) + 200 >= nfp(b.style.top)) {
+            speedx *= -1;
+            var audio = new Audio();
+            audio.src = 'ping.mp3';
+            audio.autoplay = true;
+        } else if (nfp(b.style.left) >= w - 20)
+            goal('left');
+    }
+
+
+
+
+    if (nfp(b.style.left) <= 30) {
+        if (nfp(l.style.top) <= nfp(b.style.top) + 20 && nfp(l.style.top) + 200 >= nfp(b.style.top)) {
+            speedx *= -1;
+            var audio = new Audio();
+            audio.src = 'ping.mp3';
+            audio.autoplay = true;
+        } else if (nfp(b.style.left) <= 0)
+            goal('right');
+    }
+
+
+
+    setTimeout(function() {
+        moveball()
+    }, balltime);
+}
+
+
+
+
+setInterval(function() {
+    keydown();
+}, 10);
+moveball();
+
+function goal(pos) {
+
+    ogoal.style.color = "white";
+
+    setTimeout(function() {
+        ogoal.style.color = "black"
+    }, 1000);
+
+    if (pos == "left")
+        rscore.innerHTML = Number(rscore.innerHTML) + 1;
+    else
+        lscore.innerHTML = Number(lscore.innerHTML) + 1;
+
+    var audio = new Audio();
+    audio.src = 'goal.mp3';
+    audio.autoplay = true;
+
+    speedx *= -1;
+    b.style.left = w / 2 + "px";
+}
